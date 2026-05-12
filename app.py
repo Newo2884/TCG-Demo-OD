b69919894dc649306189081cf8ebd0331f7a697f
Newo2884 <ouen.doson@gmail.com>
2026-05-12 11:45:16 +1000

    added auth and main blueprint functionality in app.py

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from models import db, User, Card, Collection
 import json, os
+
+from main.main import main_bp
+from auth.auth import auth_bp
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "instance", "tcg.db")
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(main_bp)
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html"), 404
 
 def seed_database():
     """Import card data from JSON into the SQL database."""
