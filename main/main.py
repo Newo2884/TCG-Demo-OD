875f4e70738d18862c91398a4a7bc6d43d01e04f
Newo2884 <ouen.doson@gmail.com>
2026-05-19 11:57:54 +1000

    added url_prefix to main.py

--- main/main.py
+++ main/main.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 from models import *
 
-main_bp = Blueprint("main", __name__, template_folder="templates")
+main_bp = Blueprint("main", __name__, url_prefix="/main", template_folder="templates")
 
 @main_bp.route('/index', methods=['GET', 'POST'])
 def index():
