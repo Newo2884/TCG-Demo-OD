5952299ca5d0c910fab76a81ff7b42dae76ec7b2
Newo2884 <ouen.doson@gmail.com>
2026-05-29 11:43:02 +1000

    fixed directories and finished all correct changes from magic to yu-gi-oh

--- main/main.py
+++ main/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, jsonify
 from flask_login import login_required, current_user
 from models import *
 
@@ -7,3 +7,31 @@ main_bp = Blueprint("main", __name__, url_prefix="/main", template_folder="templ
 @main_bp.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
+
+@main_bp.route("/add_to_collection", methods=["POST"])
+def add_to_collection():
+    data = request.json
+    user_id = current_user.id
+    card_name = data.get("card_name")
+
+    user = User.query.get(user_id)
+    card = Card.query.filter_by(name=card_name).first()
+
+    if not user or not card:
+        return jsonify({"success": False, "error": "User or card not found"}), 404
+
+    # Check if the user already has this card in their collection
+    existing_entry = Collection.query.filter_by(user_id=user_id, card_id=card.id).first()
+    if existing_entry:
+        existing_entry.quantity += 1
+    else:
+        new_entry = Collection(user_id=user_id, card_id=card.id)
+        db.session.add(new_entry)
+
+    db.session.commit()
+    return jsonify({"success": True, "message": "Card added to collection"})
+
+@main_bp.route("/collection/<int:user_id>", methods=["GET"])
+def view_collection(user_id):
+    c = Collection.query.filter_by(user_id=user_id).all()
+    return render_template("collection.html", collection = c)
